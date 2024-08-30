7203e866f8fb12354d37f64d3846c080b0dd1afa
AntonioCarrenoF <antonio234@icloud.com>
2024-08-29 20:15:28 -0600

    Modificación disparo bloqueado
    Se modificó la lógica para poder disparar hasta que la bala anterior frene su trayectoria

--- Artillery/Assets/_Scripts/Canon.cs
+++ Artillery/Assets/_Scripts/Canon.cs
@@ -63,21 +63,21 @@ public class Canon : MonoBehaviour
 
     private void Disparar(InputAction.CallbackContext context)
     {
-        disparos++;
-        GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
-        Rigidbody tempRB = temp.GetComponent<Rigidbody>();
-        SeguirCamara.objetivo = temp;
-        Vector3 direccionDisparo = transform.rotation.eulerAngles;
-        direccionDisparo.y = 90 - direccionDisparo.x;
-        Vector3 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
-        GameObject Particulas = Instantiate(ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
-        tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.VelocidadBala;
-        AdministradorJuego.DisparosPorJuego--;
-        SourceDisparo.Play();
-        Bloqueado = true;
-        if (AdministradorJuego.DisparosPorJuego<0)
+        if (AdministradorJuego.DisparosPorJuego>0 && Bloqueado==false)
         {
-            disparar.Disable();
+            disparos++;
+            GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+            Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+            SeguirCamara.objetivo = temp;
+            Vector3 direccionDisparo = transform.rotation.eulerAngles;
+            direccionDisparo.y = 90 - direccionDisparo.x;
+            Vector3 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
+            GameObject Particulas = Instantiate(ParticulasDisparo, puntaCanon.transform.position, Quaternion.Euler(direccionParticulas), transform);
+            tempRB.velocity = direccionDisparo.normalized * AdministradorJuego.VelocidadBala;
+            AdministradorJuego.DisparosPorJuego--;
+            SourceDisparo.Play();
+            Bloqueado = true;
+
         }
 
     }
